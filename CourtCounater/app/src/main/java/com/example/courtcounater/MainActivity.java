909914e68e8179016f5d5f9a8dcd1f9eb27e31bc
Mohammad Khan <mohammad.buet@gmail.com>
2022-01-25 19:34:50 +0900

    lesson 3 init

---
+++ CourtCounater/app/src/main/java/com/example/courtcounater/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.courtcounater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayForTeamA(8);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
