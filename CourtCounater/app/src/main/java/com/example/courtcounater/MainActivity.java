e74829df8639b5fcbb3c49e0ccd92ff37f5b6cd1
Mohammad Khan <mohammad.buet@gmail.com>
2022-01-25 21:05:45 +0900

    added team B

--- CourtCounater/app/src/main/java/com/example/courtcounater/MainActivity.java
+++ CourtCounater/app/src/main/java/com/example/courtcounater/MainActivity.java
@@ -3,15 +3,60 @@ package com.example.courtcounater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
+        // displayForTeamA(8);
+    }
+
+    /**
+     * For team A
+     */
+    public void addThreeForTeamA(View v) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+    public void addTwoForTeamA(View v) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+    public void addOneForTeamA(View v) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * For team B
+     */
+    public void addThreeForTeamB(View v) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+    public void addTwoForTeamB(View v) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+    public void addOneForTeamB(View v) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * score reset
+     */
+    public void resetScore(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
     /**
@@ -19,6 +64,14 @@ public class MainActivity extends AppCompatActivity {
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 }
