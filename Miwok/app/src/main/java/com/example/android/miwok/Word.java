4b3a6d68d3d97665b0137c0c1eae58ea4178a2ae
Mohammad Khan <mohammad.buet@gmail.com>
2022-03-02 23:11:50 +0900

    word class updated

--- Miwok/app/src/main/java/com/example/android/miwok/Word.java
+++ Miwok/app/src/main/java/com/example/android/miwok/Word.java
@@ -2,7 +2,7 @@ package com.example.android.miwok;
 
 /**
  * {@link Word} represents a vocabulary word that the user wants to learn.
- * It contains a default translation and a Miwok translation for that word.
+ * It contains a default translation, a Miwok translation, and an image for that word.
  */
 public class Word {
 
@@ -11,6 +11,9 @@ public class Word {
 
     /** Miwok translation for the word */
     private String mMiwokTranslation;
+
+    /** Image resource ID for the word */
+    private int mImageResourceId;
 
     /**
      * Create a new Word object.
@@ -25,6 +28,21 @@ public class Word {
     }
 
     /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     * @param imageResourceId is the drawable resource ID for the image associated with the word
+     *
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
+
+    /**
      * Get the default translation of the word.
      */
     public String getDefaultTranslation() {
@@ -36,6 +54,13 @@ public class Word {
      */
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    /**
+     * Return the image resource ID of the word.
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
     }
 
 }
