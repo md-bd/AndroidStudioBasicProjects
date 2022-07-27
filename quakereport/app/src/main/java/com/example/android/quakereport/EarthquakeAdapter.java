d0ee5fedcd890b8d05dc5113228e2535b33f6193
Mohammad Khan <mohammad.buet@gmail.com>
2022-07-28 02:29:04 +0900

    quakereport custom adapter added

---
+++ quakereport/app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -0,0 +1,48 @@
+package com.example.android.quakereport;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+
+    public EarthquakeAdapter(Activity context, ArrayList<Earthquake> earthquakes) {
+
+        super(context, 0, earthquakes);
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.earthquake_list_item, parent, false);
+        }
+
+        Earthquake currentEarthquake = getItem(position);
+
+        TextView magnitudeView = (TextView) listItemView.findViewById(R.id.magnitude);
+        magnitudeView.setText(currentEarthquake.getMagnitude());
+
+
+        TextView locationView = (TextView) listItemView.findViewById(R.id.location);
+        locationView.setText(currentEarthquake.getLocation());
+
+        TextView dateView = (TextView) listItemView.findViewById(R.id.date);
+        dateView.setText(currentEarthquake.getDate());
+
+        return listItemView;
+
+    }
+}
