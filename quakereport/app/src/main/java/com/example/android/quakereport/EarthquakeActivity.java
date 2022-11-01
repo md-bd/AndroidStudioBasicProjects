1b07ee188e96221430fdb7e8eb5ee7e6cb8fecc4
Mohammad Khan <mohammad.buet@gmail.com>
2022-11-01 18:59:18 +0900

    json query added

--- quakereport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ quakereport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -34,15 +34,18 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2, 2016"));
-        earthquakes.add(new Earthquake("6.1", "London", "Feb 2, 2017"));
-        earthquakes.add(new Earthquake("7.2", "Tokyo", "Nov 20, 2011"));
-        earthquakes.add(new Earthquake("3.9", "Mexico City", "Feb 2, 1017"));
-        earthquakes.add(new Earthquake("2.8", "Moscow", "Aug 19, 2012"));
-        earthquakes.add(new Earthquake("1.7", "Paris", "Oct 30, 2011"));
+//        ArrayList<Earthquake> earthquakes = new ArrayList<>();
+//        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2, 2016"));
+//        earthquakes.add(new Earthquake("6.1", "London", "Feb 2, 2017"));
+//        earthquakes.add(new Earthquake("7.2", "Tokyo", "Nov 20, 2011"));
+//        earthquakes.add(new Earthquake("3.9", "Mexico City", "Feb 2, 1017"));
+//        earthquakes.add(new Earthquake("2.8", "Moscow", "Aug 19, 2012"));
+//        earthquakes.add(new Earthquake("1.7", "Paris", "Oct 30, 2011"));
 
-        // Find a reference to the {@link ListView} in the layout
+        //
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+
+                // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a new {@link ArrayAdapter} of earthquakes
