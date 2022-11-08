1eb2c91726cb6999fde55c0abf6e788f8046ef8b
Mohammad Khan <mohammad.buet@gmail.com>
2022-11-09 01:15:12 +0900

    add earthquake intent

--- quakereport/app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ quakereport/app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -14,18 +14,23 @@ public class Earthquake {
     /** Time of the earthquake */
     private long mTimeInMilliseconds;
 
+    /** Website URL of the earthquake */
+    private String mUrl;
+
     /**
      * Constructs a new {@link Earthquake} object.
      *
      * @param magnitude is the magnitude (size) of the earthquake
-     * @param location is the city location of the earthquake
+     * @param location is the location where the earthquake happened
      * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
-     *  earthquake happened
+     *                           earthquake happened
+     * @param url is the website URL to find more details about the earthquake
      */
-    public Earthquake(double magnitude, String location, long timeInMilliseconds) {
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
         mMagnitude = magnitude;
         mLocation = location;
         mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
     }
 
     /**
@@ -50,5 +55,11 @@ public class Earthquake {
         return mTimeInMilliseconds;
     }
 
+    /**
+     * Returns the website URL to find more information about the earthquake.
+     */
+    public String getUrl() {
+        return mUrl;
+    }
 
 }
